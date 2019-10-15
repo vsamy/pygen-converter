13a4f09e93d0b0c72175453451a5c3cd1e5a55b8
Vincent Samy <vincent.samy@aist.go.jp>
2019-10-15 13:54:41 +0900

    Relax license.

--- tests/lib/bindings.cpp
+++ tests/lib/bindings.cpp
@@ -1,21 +1,25 @@
-/*      File: bindings.cpp
-*       This file is part of the program pygen-converter
-*       Program description : This is an header only library that provides list to eigen and numpy to eigen converters for boost python.
-*       Copyright (C) 2017 -  Vincent Samy (LIRMM). All Right reserved.
-*
-*       This software is free software: you can redistribute it and/or modify
-*       it under the terms of the CeCILL-C license as published by
-*       the CEA CNRS INRIA, either version 1
-*       of the License, or (at your option) any later version.
-*       This software is distributed in the hope that it will be useful,
-*       but WITHOUT ANY WARRANTY without even the implied warranty of
-*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*       CeCILL-C License for more details.
-*
-*       You should have received a copy of the CeCILL-C License
-*       along with this software. If not, it can be found on the official website
-*       of the CeCILL licenses family (http://www.cecill.info/index.en.html).
-*/
+// MIT License
+
+// Copyright (c) 2019 Vincent SAMY
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "functions.h"
 #include "converters.h"
 #include <boost/python.hpp>
